2f99044d44ec7bdd2760d2a30cf59979f00a3987
Bharadwaj P <barbi.bruce@gmail.com>
2011-06-22 13:29:51 -0500

    Added readme and code cleanup

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Yipit deals is a command line utility to check for yipit deals.
+
+Usage:
+python yipit.py key1=value1 key2=value2
+keys as found in yipit API documention page 
+http://yipit.com/about/api/documentation
+
+Example:
+python yipit.py division=minneapolis,st-paul source=groupon
+
--- yipit.py
+++ yipit.py
@@ -1,31 +1,30 @@
 import sys
-import urllib, urllib2
+import urllib
+import urllib2
 import json
 
-# division=new-york source=groupon tag=massage
 
 class yipit_deals(object):
     """A command line utility to search yipit deals"""
-    
+
     api_key = 'zH4RS53tEN6C9PM5'
     yipit_api_url = 'http://api.yipit.com/v1/'
     deals_resource_path = 'deals/?'
     limit = 20
-    
+
     params = {}
-    
+
     def parse_inputs(self, args):
         """Receive comma seperated inputs from command line"""
-        print 'parsing inputs'
         for arg in args:
             print arg
-            key,value = arg.split('=')
+            key, value = arg.split('=')
             self.params[key] = value
 
     def sanitize_input(self):
         """Check for valid input params"""
         pass
-    
+
     def api_call(self):
         """Make the actual API call"""
         url = self.yipit_api_url + self.deals_resource_path
@@ -34,7 +33,7 @@ class yipit_deals(object):
         req = urllib2.Request(url)
         response = urllib2.urlopen(req)
         return response.read()
-    
+
     def parse_output(self, json_string):
         """Parse API json response"""
         deals = json.loads(json_string)
@@ -46,17 +45,14 @@ class yipit_deals(object):
         deals = deals[:self.limit]
         for deal in deals:
             print deal['title']
-        
 
     def main(self, args):
         """Actual process"""
-        print 'I am in yipit_deals main'
         self.parse_inputs(args)
         response = self.api_call()
         deals = self.parse_output(response)
         self.show_deals(deals['response']['deals'])
 
 if __name__ == '__main__':
-    print 'I am in yipit main'
     y = yipit_deals()
     y.main(sys.argv[1:])
